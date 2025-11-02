20ed5feaaec19744b4dd242179fe53d1e083b800
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-11-02 18:27:19 -0500

    Adding Map Section

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
-
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -10,28 +8,28 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-
 if(navigator.geolocation){
   //Accesses Geolocation API 
   navigator.geolocation.getCurrentPosition(
     function(position){
       //Destructuring The Position Object - Success
-      const {latitude} = position.coords.latitude
-      const {longitude} = position.coords.longitude
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
-      const map = L.map('map').setView([51.505, -0.09], 13);
- 
+      const {latitude, longitude} = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      console.log(coords);
+      
+      const map = L.map('map').setView(coords, 13);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
-
-      L.marker([51.5, -0.09]).addTo(map)
-          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-          .openPopup();
-          }, 
+      
+      L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    }, 
     function(){
       //Error Message - Failed 
-     alert('Could Not Get Your Location') 
+      alert('Could Not Get Your Location');
     }
   );
 }
