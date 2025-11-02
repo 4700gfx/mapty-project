610bfeae4efb04f4668fe98fdb31220749b20dd7
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-11-02 18:27:44 -0500

    Adding New Theme

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ if(navigator.geolocation){
       console.log(coords);
       
       const map = L.map('map').setView(coords, 13);
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
       
