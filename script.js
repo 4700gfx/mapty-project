168e85ee827f48338a2f1d5dd3ff9a77e1a40615
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-11-02 17:47:32 -0500

    Adding Leaflet Library

--- script.js
+++ script.js
@@ -18,8 +18,17 @@ if(navigator.geolocation){
       //Destructuring The Position Object - Success
       const {latitude} = position.coords.latitude
       const {longitude} = position.coords.longitude
-      console.log(latitude, longitude)
-    }, 
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
+      const map = L.map('map').setView([51.505, -0.09], 13);
+ 
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+      L.marker([51.5, -0.09]).addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+          }, 
     function(){
       //Error Message - Failed 
      alert('Could Not Get Your Location') 
